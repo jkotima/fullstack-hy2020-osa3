0b0c057f6cff3fb12b951fb5fb37941c286cce5c
jkotima <jkotima@gmail.com>
2020-06-11 19:21:57 +0300

    3.11

---
+++ build/precache-manifest.fc641880370816803252fb1e13082ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf4f2e7b4fca0ce05aa2d40ab4918da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eba5018c8a6bb3ddb29",
+    "url": "/static/css/main.b0a7673b.chunk.css"
+  },
+  {
+    "revision": "25ee9fd4b9873ea818b9",
+    "url": "/static/js/2.90859df1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90859df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eba5018c8a6bb3ddb29",
+    "url": "/static/js/main.35b3f2f6.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
